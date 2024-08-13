de235c788592f6e69b1d4bb04420ea636fce8247
kaito <4681kaito@gmail.com>
2024-08-14 04:56:05 +0900

    [FIx]　ログインボタンの自動クリック機能の削除（自動クリックは反応しなかったため）

--- src/setting/setting.js
+++ src/setting/setting.js
@@ -14,24 +14,3 @@ document.getElementById("textButton").addEventListener("click", () => {
 		});
 	}
 });
-
-//autoLogin
-//初期状態
-(async () => {
-	const result = await chrome.storage.sync.get(["autoLogin"]);
-	const autoLogin = result.autoLogin;
-	if(autoLogin === true){
-		document.getElementById('autoLogin').checked = true;
-	}
-})();
-
-
-// チェックボックスの状態が変わったときの処理
-document.getElementById('autoLogin').addEventListener('change', function () {
-	chrome.storage.sync.set({ autoLogin: this.checked }, () => {
-		if (chrome.runtime.lastError) {
-			this.checked = !this.checked;
-			alert("エラーが発生しました。もう一度実行してください。");
-		}
-	});
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,51 @@
+// const path = require('path');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// module.exports = {
+// 	mode: 'production',
+// 	entry: {
+// 		autofill: './src/contents/autofill.js',
+// 		registerKey: './src/contents/registerKey.js',
+// 		popup: './src/popup/popup.js'
+// 	},
+// 	output: {
+// 		filename: (pathData) => {
+// 			// エントリーポイントの名前によって出力先のパスを決定
+// 			if (pathData.chunk.name === 'popup') {
+// 				return 'popup/[name].js';
+// 			}
+// 			return 'contents/[name].js';
+// 		},
+// 		path: path.resolve(__dirname, 'dist/')
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /node_modules/,
+// 				use: {
+// 					loader: 'babel-loader',
+// 					options: {
+// 						presets: ['@babel/preset-env']
+// 					}
+// 				}
+// 			}
+// 		]
+// 	},
+// 	plugins: [
+// 		new CopyWebpackPlugin({
+// 			patterns: [
+// 				{ from: 'src/manifest.json', to: '' },
+// 				{ from: 'src/popup/popup.html', to: './popup/' },
+// 				{ from: 'src/popup/popup.css', to: './popup/' },
+// 				{ from: 'src/setting/*', to: './setting/' },
+// 				{ from: 'src/instruction/*', to: 'instruction/' },
+// 				{ from: 'src/contents/autoAgree.js', to: 'contents/' },
+// 				{ from: 'src/contents/autoLogin.js', to: 'contents/' },
+// 			]
+// 		})
+// 	]
+// };
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -35,14 +83,13 @@ module.exports = {
 	plugins: [
 		new CopyWebpackPlugin({
 			patterns: [
-				{ from: 'src/manifest.json', to: '' },
-				{ from: 'src/popup/popup.html', to: 'popup/' },
-				{ from: 'src/popup/popup.css', to: 'popup/' },
-				{ from: 'src/setting/setting.html', to: 'setting/' },
-				{ from: 'src/setting/setting.css', to: 'setting/' },
-				{ from: 'src/setting/setting.js', to: 'setting/' },
-				{ from: 'src/contents/autoAgree.js', to: 'contents/' },
-				{ from: 'src/contents/autoLogin.js', to: 'contents/' },
+				{ from: path.resolve(__dirname, 'src/manifest.json'), to: path.resolve(__dirname, 'dist/') },
+				{ from: path.resolve(__dirname, 'src/popup/popup.html'), to: path.resolve(__dirname, 'dist/popup/') },
+				{ from: path.resolve(__dirname, 'src/popup/popup.css'), to: path.resolve(__dirname, 'dist/popup/') },
+				{ from: path.resolve(__dirname, 'src/setting/'), to: path.resolve(__dirname, 'dist/setting/'), globOptions: { dot: true } }, 
+				{ from: path.resolve(__dirname, 'src/instruction/'), to: path.resolve(__dirname, 'dist/instruction/'), globOptions: { dot: true } }, 
+				{ from: path.resolve(__dirname, 'src/contents/autoAgree.js'), to: path.resolve(__dirname, 'dist/contents/') },
+				{ from: path.resolve(__dirname, 'src/contents/autoLogin.js'), to: path.resolve(__dirname, 'dist/contents/') },
 			]
 		})
 	]

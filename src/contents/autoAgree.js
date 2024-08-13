10326e9aa86381d2db322247b2e7312c678ae9f5
kaito <4681kaito@gmail.com>
2024-08-14 03:19:15 +0900

    [Fix] 送信属性選択画面のDOM変更に対応

--- src/contents/autoAgree.js
+++ src/contents/autoAgree.js
@@ -3,7 +3,33 @@
 // https://ufidp-t.cii.u-fukui.ac.jp/idp/profile/SAML2/* にアクセス時に実行
 // 送信属性の選択画面だったら、同意ボタンを自動で押す。
 // 
-if (document.getElementById("#generalConsentDiv")) {
-	const button = document.querySelector('input[type="submit"][name="_eventId_proceed"][value="同意"]');
-	if (button) button.click();
+async function autoAgree() {
+	// 要素が表示されたら実行する関数
+	const onElementFound = async (element) => {
+		const button = document.querySelector('input[type="submit"][name="_eventId_proceed"][value="同意"]');
+		if (button) button.click();
+	};
+
+	// DOMの変更の監視
+	const observer = new MutationObserver(async (mutationsList) => {
+		for (const mutation of mutationsList) {
+			if (mutation.type === 'childList') {
+				const element = document.getElementById("generalConsentDiv");
+				if (element) {
+					await onElementFound(element);
+				}
+			}
+		}
+	});
+
+	// DOM変更前も調べる
+	const sendAttribute = document.getElementById("generalConsentDiv");
+	if (sendAttribute) {
+		const button = document.querySelector('input[type="submit"][name="_eventId_proceed"][value="同意"]');
+		if (button) button.click();
+	}
+	// 監視を開始する
+	observer.observe(document.body, { childList: true, subtree: true });
 }
+
+autoAgree();
